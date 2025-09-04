d6910da7bdc5e34c98e048d89497a1178f7cd5ce
dungeonzalz <malikanoer1988@gmail.com>
2025-09-04 08:52:32 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,17 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  root: path.resolve(__dirname, "client"),
-  publicDir: path.resolve(__dirname, "client/public"),
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "client/src"),
       "@shared": path.resolve(__dirname, "shared"),
-      "@assets": path.resolve(__dirname, "attached_assets"),
-    },
+      "@assets": path.resolve(__dirname, "attached_assets")
+    }
   },
+  root: path.resolve(__dirname, "client"),
   build: {
     outDir: path.resolve(__dirname, "dist"),
-    emptyOutDir: true,
-  },
-  server: {
-    port: 5173,
-  },
+    emptyOutDir: true
+  }
 });
